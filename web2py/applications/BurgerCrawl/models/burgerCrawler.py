f150c08c6a1b9ca39f5eda35be0bbcc104a01f07
Joel <samuelsson.joel@gmail.com>
2014-10-13 21:06:01 +0200

    - some updates

--- web2py/applications/BurgerCrawl/models/burgerCrawler.py
+++ web2py/applications/BurgerCrawl/models/burgerCrawler.py
@@ -16,6 +16,8 @@ class BurgerCrawler(object):
 					url, restaurant = line.split(" ")
 					restaurants.append((url.strip(), restaurant.strip()))
 
+        return restaurants
+
 	def addRestaurant(self, url, restaurant):
 		with open("sites.txt", "a") as sites:
 			sites.writeline(url + " " + restaurant)
@@ -37,20 +39,4 @@ class BurgerCrawler(object):
 						matches.append(match)
 
 		return matches
-
-#			 SITES = [("http://hamnpaviljongen.com/lunchmeny/", "Hamnpaviljongen"),
-#					  ("http://www.kadbano.se/index.php/lunch", "Kadbano"),
-#					  ("http://www.rmagnussons.se/index.php/lunch", "Magnussons"),
-#					  ("http://www.villaromana.se/index.php/se/lunchmeny", "Villa Romana"),
-#					  ("http://www.kvartersmenyn.se/start/rest/13682", "Sherlocks"),
-#					  ("http://www.xn--vrdshusetliljan-0kb.se/index.php/lunch", "Värdshuset Liljan"),
-#					  ("http://www.kvartersmenyn.se/start/rest/10511", "Börjes"),
-#					  ("http://fagelsangen.kvartersmenyn.se/", "Fågelsången"),
-#					  ("http://borgenuppsala.se/dagens-lunch-i-uppsala/", "Borgen"),
-#					  ("http://domtrappkallaren.se/dagens-lunch-i-uppsala/", "Domtrappkällaren"),
-#					  ("http://www.unt.se/Page.aspx?url=lunchguiden%2f&ShowInfo=1&RestID=1947851", "Lucillus"),
-#					  ("http://www.unt.se/Page.aspx?url=lunchguiden%2f&ShowInfo=1&RestID=1947819", "Picnic"),
-#					  ("http://www.unt.se/Page.aspx?url=lunchguiden%2f&ShowInfo=1&RestID=1947650", "Stationen"),
-#					  ("http://www.ukk.se/Restaurang-cafe/Veckans-luncher/", "UKK"),
-#					  ("http://www.unt.se/Page.aspx?url=lunchguiden%2f&ShowInfo=1&RestID=1947780", "Åkanten")]
 	
