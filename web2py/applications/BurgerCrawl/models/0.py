1215cbf2c25b8e0ce4c550e3d557f496f0bf162b
Joel <samuelsson.joel@gmail.com>
2014-10-13 18:05:53 +0200

    - initial commit of burgercrawl

---
+++ web2py/applications/BurgerCrawl/models/0.py
@@ -0,0 +1,19 @@
+from gluon.storage import Storage
+settings = Storage()
+
+settings.migrate = True
+settings.title = 'BurgerCrawl'
+settings.subtitle = ''
+settings.author = 'Joel Samuelsson'
+settings.author_email = ''
+settings.keywords = ''
+settings.description = ''
+settings.layout_theme = 'Default'
+settings.database_uri = 'sqlite://storage.sqlite'
+settings.security_key = '98b7fbb4-bb7a-4228-a5a1-e2ee845ed97a'
+settings.email_server = ''
+settings.email_sender = ''
+settings.email_login = ''
+settings.login_method = 'local'
+settings.login_config = ''
+settings.plugins = []
